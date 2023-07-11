be8669a80aa67c5c410200d06a0fcefbaf0de3cc
Paras <dparas369@gmail.com>
2023-07-11 15:45:33 +0530

    HomeScreen work

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,15 +1,26 @@
-import { Dimensions, StyleSheet, Text, View, ViewBase } from "react-native";
+import {
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  ViewBase,
+} from "react-native";
 import React from "react";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.itemButton}>ADD NEW NOTE</Text>
-      </View>
-      <View>
-        <Text style={styles.itemButton}>VIEW ALL NOTES</Text>
-      </View>
+      <Pressable>
+        <View style={styles.itemButton}>
+          <Text>ADD NEW NOTE</Text>
+        </View>
+      </Pressable>
+      <Pressable>
+        <View style={styles.itemButton}>
+          <Text>VIEW ALL NOTES</Text>
+        </View>
+      </Pressable>
     </View>
   );
 };
@@ -24,6 +35,11 @@ const styles = StyleSheet.create({
     marginTop: 40,
   },
   itemButton: {
-    width: Dimensions.get('window').width-100,
+    width: Dimensions.get("window").width - 100,
+    height: "50%",
+    marginVertical: 30,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
