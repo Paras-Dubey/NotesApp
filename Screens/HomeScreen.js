fbee8c887e0d0f4cff1aea4d0fbb78ed754fc6d6
Paras <dparas369@gmail.com>
2023-07-11 21:05:54 +0530

    HomeScreen work

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -7,18 +7,21 @@ import {
   ViewBase,
 } from "react-native";
 import React from "react";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Pressable>
         <View style={styles.itemButton}>
-          <Text>ADD NEW NOTE</Text>
+          <Text style={styles.title}>ADD NEW NOTE</Text>
+          <Icon style={styles.icon} name="arrow-forward-outline" />
         </View>
       </Pressable>
       <Pressable>
         <View style={styles.itemButton}>
-          <Text>VIEW ALL NOTES</Text>
+          <Text style={styles.title}>VIEW ALL NOTES</Text>
+          <Icon style={styles.icon} name="arrow-forward-outline" />
         </View>
       </Pressable>
     </View>
@@ -41,5 +44,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#f5f5f5",
     justifyContent: "center",
     alignItems: "center",
+    elevation: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  icon: {
+    fontSize: 30,
+    paddingTop: 20,
   },
 });
